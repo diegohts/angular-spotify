323fc087ec9eaccedae7d9aa125a3c428e30c7b1
Diego Henrique <diegohrp20@gmail.com>
2022-04-25 01:50:41 -0300

    Initial Commit. Structuring the project and establishing the route for the login component.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,22 @@ export const environment = {
   production: false
 };
 
+export const SpotifyConfiguration = {
+  clientId: 'e75a66215d1a4f32a2e075517b7bbd84',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
